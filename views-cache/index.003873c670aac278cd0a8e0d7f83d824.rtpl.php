3cccbfaa33d8be1b92c23384a11bfd5adc552fea
Marcia <marciasprates@hotmail.com>
2019-06-24 15:09:24 -0300

    TPL Site

--- views-cache/index.003873c670aac278cd0a8e0d7f83d824.rtpl.php
+++ views-cache/index.003873c670aac278cd0a8e0d7f83d824.rtpl.php
@@ -1,1 +1,172 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>Olá!</h1>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!-- Hero section -->
+	<section class="hero-section">
+		<div class="hero-slider owl-carousel">
+			<div class="hs-item set-bg" data-setbg="custom/img/bg.jpg">
+				<div class="container">
+					<div class="row">
+						<div class="col-xl-6 col-lg-7 text-white">
+							<span>Jaqueta Jeans</span>
+							<h2>Denim jackets</h2>
+							<p>Moderna, bonita e estilosa. </p>
+							<a href="#" class="site-btn sb-white">ADICIONAR AO CARRINHO</a>
+						</div>
+					</div>
+					<div class="offer-card text-white">
+						<span>Apenas</span>
+						<h2>$30</h2>
+						<p>PROMOÇÃO</p>
+					</div>
+				</div>
+			</div>
+			<div class="hs-item set-bg" data-setbg="custom/img/bg-2.jpg">
+				<div class="container">
+					<div class="row">
+						<div class="col-xl-6 col-lg-7 text-white">
+							<span>Óculos Retrô</span>
+							<h2>Ana Hickmann</h2>
+							<p>Aros prateados, hastes de silicone, estiloso. </p>
+							<a href="#" class="site-btn sb-white">ADICIONAR AO CARRINHO</a>
+						</div>
+					</div>
+					<div class="offer-card text-white">
+						<span>Apenas</span>
+						<h2>$29</h2>
+						<p>PROMOÇÃO</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="slide-num-holder" id="snh-1"></div>
+		</div>
+	</section>
+	<!-- Hero section end -->
+
+
+
+	<!-- Features section -->
+	<section class="features-section">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-4 p-0 feature">
+					<div class="feature-inner">
+						<div class="feature-icon">
+							<img src="custom/img/icons/1.png" alt="#">
+						</div>
+						<h2>1 ano de garantia</h2>
+					</div>
+				</div>
+				<div class="col-md-4 p-0 feature">
+					<div class="feature-inner">
+						<div class="feature-icon">
+							<img src="custom/img/icons/2.png" alt="#">
+						</div>
+						<h2>Frete Grátis</h2>
+					</div>
+				</div>
+				<div class="col-md-4 p-0 feature">
+					<div class="feature-inner">
+						<div class="feature-icon">
+							<img src="custom/img/icons/3.png" alt="#">
+						</div>
+						<h2>Pagamento Seguro</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Features section end -->
+
+
+	<!-- letest product section -->
+	<section class="top-letest-product-section" id="Produtos">
+		<div class="container">
+			<div class="section-title">
+				<h2>PRODUTOS</h2>
+			</div>
+			<div class="product-slider owl-carousel">
+				<div class="product-item">
+					<div class="pi-pic">
+						<img src="custom/img/product/1.jpg" alt="">
+						<div class="pi-links">
+							<a href="#" class="add-card"><i class="flaticon-bag"></i><span>CARRINHO</span></a>
+							<a href="#" class="wishlist-btn"><i class="flaticon-heart"></i></a>
+						</div>
+					</div>
+					<div class="pi-text">
+						<h6>$35,00</h6>
+						<p>Flamboyant Pink Top </p>
+					</div>
+				</div>
+				<div class="product-item">
+					<div class="pi-pic">
+						<div class="tag-new">New</div>
+						<img src="custom/img/product/2.jpg" alt="">
+						<div class="pi-links">
+							<a href="#" class="add-card"><i class="flaticon-bag"></i><span>CARRINHO</span></a>
+							<a href="#" class="wishlist-btn"><i class="flaticon-heart"></i></a>
+						</div>
+					</div>
+					<div class="pi-text">
+						<h6>$35,00</h6>
+						<p>Black and White Stripes Dress</p>
+					</div>
+				</div>
+				<div class="product-item">
+					<div class="pi-pic">
+						<img src="custom/img/product/3.jpg" alt="">
+						<div class="pi-links">
+							<a href="#" class="add-card"><i class="flaticon-bag"></i><span>CARRINHO</span></a>
+							<a href="#" class="wishlist-btn"><i class="flaticon-heart"></i></a>
+						</div>
+					</div>
+					<div class="pi-text">
+						<h6>$35,00</h6>
+						<p>Flamboyant Pink Top </p>
+					</div>
+				</div>
+				<div class="product-item">
+						<div class="pi-pic">
+							<img src="custom/img/product/4.jpg" alt="">
+							<div class="pi-links">
+								<a href="#" class="add-card"><i class="flaticon-bag"></i><span>CARRINHO</span></a>
+								<a href="#" class="wishlist-btn"><i class="flaticon-heart"></i></a>
+							</div>
+						</div>
+						<div class="pi-text">
+							<h6>$35,00</h6>
+							<p>Flamboyant Pink Top </p>
+						</div>
+					</div>
+				<div class="product-item">
+						<div class="pi-pic">
+							<img src="custom/img/product/6.jpg" alt="">
+							<div class="pi-links">
+								<a href="#" class="add-card"><i class="flaticon-bag"></i><span>CARRINHO</span></a>
+								<a href="#" class="wishlist-btn"><i class="flaticon-heart"></i></a>
+							</div>
+						</div>
+						<div class="pi-text">
+							<h6>$35,00</h6>
+							<p>Flamboyant Pink Top </p>
+						</div>
+					</div>
+			</div>
+		</div>
+	</section>
+	<!-- letest product section end -->
+
+
+
+	<!-- Banner section -->
+	<section class="banner-section">
+		<div class="container">
+			<div class="banner set-bg" data-setbg="custom/img/banner-bg.jpg">
+				<div class="tag-new">EXCLUSIVO</div>
+				<span>Produtos com</span>
+				<h2>EXCELÊNCIA</h2>
+				<a href="#Produtos" class="site-btn">É só aqui!</a>
+			</div>
+		</div>
+	</section>
+	<!-- Banner section end  -->
