d4d0a361ba3f4261d3d45cf26b27c2482e1525dc
Marcia <marciasprates@hotmail.com>
2019-06-28 15:16:00 -0300

    Valid Forgot Decrypt

--- vendor/hcodebr/php-classes/src/Model/user.php
+++ vendor/hcodebr/php-classes/src/Model/user.php
@@ -12,6 +12,8 @@ class User extends Model {
 
 	const SECRET = "HcodePhp7_Secret";
 
+	const SECRET_IV = "HcodePhp7_Secret_IV";
+
 	public static function login($login, $password) {
 
 		$sql = new Sql();
@@ -175,7 +177,7 @@ class User extends Model {
 
  				$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
 
- 				$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Hcode Store", "forgot", array(
+ 				$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Loja Virtual", "forgot", array(
 					"name"=>$data["desperson"],
 					"link"=>$link
 				));
@@ -187,6 +189,38 @@ class User extends Model {
  			}
 
  		}
+
+ 	}
+
+ 	public static function validForgotDecrypt($code) {
+
+ 		$code = base64_decode($code);
+
+		$idrecovery = openssl_decrypt($code, 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_userspasswordsrecoveries a
+			INNER JOIN tb_users b USING(iduser)
+			INNER JOIN tb_persons c USING(idperson)
+			WHERE
+				a.idrecovery = :idrecovery
+				AND
+				a.dtrecovery IS NULL
+				AND
+				DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();", array(
+			":idrecovery"=>$idrecovery
+		));
+
+		if (count($results) === 0) {
+
+			throw new \Exception("Não foi possível recuperar a senha.");
+			
+		} else {
+
+			return $results[0];
+
+		}
 
  	}
 
