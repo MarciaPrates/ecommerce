3be7026956eaa6a9aae1e62acc9496635a3a1914
Marcia <marciasprates@hotmail.com>
2020-04-12 12:05:08 -0300

    Site - Alterar Senha

--- views-cache/profile-menu.003873c670aac278cd0a8e0d7f83d824.rtpl.php
+++ views-cache/profile-menu.003873c670aac278cd0a8e0d7f83d824.rtpl.php
@@ -2,7 +2,7 @@
     <h3>Menu</h3>
         <ul class="price-list">
             <a href="/profile"><li>Editar Dados</li></a>
-            <a href=""><li>Alterar Senha</li></a>
+            <a href="/profile/change-password"><li>Alterar Senha</li></a>
             <a href="/profile/orders"><li>Meus Pedidos</li></a>
             <a href="/logout"><li class="total"><button class="site-btn submit-order-btn">SAIR</button></li></a>
         </ul>
